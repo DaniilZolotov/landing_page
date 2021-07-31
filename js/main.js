5a26f14f7a4838e44e18494e6f2677834cc52e1b
Daniil <zolotovdaniil2@gmail.com>
2021-07-31 18:14:41 +0300

    Header + Section: js add

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var btn1 = document.querySelector('.left_btn')
+btn.onclick= function (){
+alert('Ещё рано')
+}
+
+var btn2 = document.querySelector('.right_btn')
+btn.onclick= function (){
+alert('Ещё рано')
+}
+
+var btn3 = document.querySelector('.first_btn')
+btn.onclick= function (){
+alert('Ещё рано')
+}
+
+var btn4 = document.querySelector('.second_btn')
+btn.onclick= function (){
+alert('Ещё рано')
+}
